602a5fcef5033605ebbfb1d570b9fa8ec390953d
s10046602 <mitchellcrainred@gmail.com>
2023-10-19 10:34:27 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from kivy.app import App
-from kivy.app import Screen
-
+from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+#from kivy.graphics.vertex_instructions import Rectangle
+#from kivy.graphics.context_instructions import Color
 
 class PhotoGalleryApp(App):
     pass
+class MouseTouch(Widget):
+    def on_touch_down(self,touch):
+        print("\nMouse Button Pressed")
+        coords = touch.pos
+        print("x coordinte: "+str(int(coords[0])))
+    def on_touch_move(self,touch):
+        print("\nMouse Moving")
+        coords = touch.pos
+        print("x coordinate: " + str())
+    def on_touch_up(self,touch):
 
 class Display(Screen):
     def display_image(self):
@@ -19,7 +31,7 @@ class Display(Screen):
 
 
 
-images = ["LoafofBread.jfif","Spork.jpg","SpongebobChair.jpg"]
+images = ["CheeseBlock.jpg","spork.jpg","SpongebobChair.jpg"]
 index = 0
 
 PhotoGalleryApp().run()
